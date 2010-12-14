52acf9c77498a0491a991233f7fc1c41916d4d5c
mythz <demis.bellot@gmail.com>
2010-12-14 12:02:59 +0000

    increment version number and change build scripts

--- build/build.bat
+++ build/build.bat
@@ -2,6 +2,6 @@
 REM SET BUILD=Debug
 SET BUILD=Release
 
-COPY ..\src\ServiceStack.Text\bin\%BUILD%\*.dll C:\src\ServiceStack\release\latest\ServiceStack.Text\
-COPY ..\src\ServiceStack.Text\bin\%BUILD%\*.dll C:\src\ServiceStack\release\lib
-COPY ..\src\ServiceStack.Text\bin\%BUILD%\*.pdb C:\src\ServiceStack\release\lib
+COPY ..\src\ServiceStack.Text\bin\%BUILD%\*.* ..\..\ServiceStack\release\latest\ServiceStack.Text\
+COPY ..\src\ServiceStack.Text\bin\%BUILD%\*.* ..\..\ServiceStack\release\lib
+COPY ..\src\ServiceStack.Text\bin\%BUILD%\*.* ..\..\ServiceStack\lib
--- src/ServiceStack.Text/Env.cs
+++ src/ServiceStack.Text/Env.cs
@@ -23,7 +23,7 @@ namespace ServiceStack.Text
 				+ (IsMonoTouch ? " MonoTouch" : "");
 		}
 
-		public static decimal ServiceStackVersion = 1.64m;
+		public static decimal ServiceStackVersion = 1.67m;
 
 		public static bool IsUnix { get; set; }
 
