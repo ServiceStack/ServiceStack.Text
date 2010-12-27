a771c66001f3f816d8384bbdd32c959dca39b5db
Demis Bellot <demis.bellot@gmail.com>
2010-12-27 13:17:59 +0000

    Add missing protobuf dlls so all tests pass, merged fix + changes of Dictionary<int,int> from @lhaussknecht - Thanks!

--- src/ServiceStack.Text/Common/WriteDictionary.cs
+++ src/ServiceStack.Text/Common/WriteDictionary.cs
@@ -108,6 +108,7 @@ namespace ServiceStack.Text.Common
 				JsWriter.WriteItemSeperatorIfRanOnce(writer, ref ranOnce);
 
 				JsState.WritingKeyCount++;
+				JsState.IsWritingValue = false;
 				writeKeyFn(writer, key);
 				JsState.WritingKeyCount--;
 
--- src/ServiceStack.Text/Env.cs
+++ src/ServiceStack.Text/Env.cs
@@ -23,7 +23,7 @@ namespace ServiceStack.Text
 				+ (IsMonoTouch ? " MonoTouch" : "");
 		}
 
-		public static decimal ServiceStackVersion = 1.69m;
+		public static decimal ServiceStackVersion = 1.70m;
 
 		public static bool IsUnix { get; set; }
 
