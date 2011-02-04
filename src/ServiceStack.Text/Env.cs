c71d2c9b4d57e22cde5707ef8701bfb66f79d2b7
Demis Bellot <demis.bellot@gmail.com>
2011-02-04 03:10:58 +0000

    upgrade version

--- src/ServiceStack.Text/Env.cs
+++ src/ServiceStack.Text/Env.cs
@@ -23,7 +23,7 @@ namespace ServiceStack.Text
 				+ (IsMonoTouch ? " MonoTouch" : "");
 		}
 
-		public static decimal ServiceStackVersion = 1.78m;
+		public static decimal ServiceStackVersion = 1.79m;
 
 		public static bool IsUnix { get; set; }
 
