36fd5ead6dabfb1582b24bd81e179c2338bf1530
Demis Bellot <demis.bellot@gmail.com>
2011-02-17 01:19:47 +0000

    split CsvConfig into a different class

---
+++ src/ServiceStack.Text/CsvConfig.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+
+namespace ServiceStack.Text
+{
+	public static class CsvConfig<T>
+	{
+		private static Dictionary<string, string> customHeadersMap;
+		public static Dictionary<string, string> CustomHeadersMap
+		{
+			get
+			{
+				return customHeadersMap;
+			}
+			set
+			{
+				customHeadersMap = value;
+				if (value == null) return;
+				CsvWriter<T>.ConfigureCustomHeaders(customHeadersMap);
+			}
+		}
+
+		public static object CustomHeaders
+		{
+			set
+			{
+				if (value == null) return;
+				if (value.GetType().IsValueType)
+					throw new ArgumentException("CustomHeaders is a ValueType");
+
+				var propertyInfos = value.GetType().GetProperties();
+				if (propertyInfos.Length == 0) return;
+
+				customHeadersMap = new Dictionary<string, string>();
+				foreach (var pi in propertyInfos)
+				{
+					var getMethod = pi.GetGetMethod();
+					if (getMethod == null) continue;
+
+					var oValue = getMethod.Invoke(value, new object[0]);
+					if (oValue == null) continue;
+					customHeadersMap[pi.Name] = oValue.ToString();
+				}
+				CsvWriter<T>.ConfigureCustomHeaders(customHeadersMap);
+			}
+		}
+
+		public static void Reset()
+		{
+			CsvWriter<T>.Reset();
+		}
+	}
+}
