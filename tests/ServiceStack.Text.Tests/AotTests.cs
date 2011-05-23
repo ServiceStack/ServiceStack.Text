bd5f208ae703a7844e374480cce9ddc26f7cb2ff
Demis Bellot <demis.bellot@gmail.com>
2011-05-23 18:48:53 +0100

    Add XML docs for Release builds and add latest xml doc to Nuget/lib

--- tests/ServiceStack.Text.Tests/AotTests.cs
+++ tests/ServiceStack.Text.Tests/AotTests.cs
@@ -6,10 +6,12 @@ namespace ServiceStack.Text.Tests
 	[TestFixture]
 	public class AotTests
 	{
+#if SILVERLIGHT || MONOTOUCH
 		[Test]
 		public void Can_Register_AOT()
 		{
-			JsConfig.RegisterForAot<Movie>();
+			JsConfig.RegisterForAot();
 		}
+#endif
 	}
 }
