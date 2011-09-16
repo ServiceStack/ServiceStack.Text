ad857f59ac2145eee6c7dd7512fb38c1c2e17c87
Rick Fleming <rfleming@acqusys.com>
2011-09-16 09:18:58 -0500

    Fix for an error with nullable valuetypes when they are null.

--- src/ServiceStack.Text/Common/DeserializeBuiltin.cs
+++ src/ServiceStack.Text/Common/DeserializeBuiltin.cs
@@ -31,43 +31,78 @@ namespace ServiceStack.Text.Common
 		private static ParseStringDelegate GetParseFn()
 		{
 			//Note the generic typeof(T) is faster than using var type = typeof(T)
-			if (typeof(T) == typeof(bool) || typeof(T) == typeof(bool?))
+			if (typeof(T) == typeof(bool))
 				return value => bool.Parse(value);
-			if (typeof(T) == typeof(byte) || typeof(T) == typeof(byte?))
+			if (typeof(T) == typeof(bool?))
+				return value => value == null ? null : (bool?) bool.Parse(value);
+			if (typeof(T) == typeof(byte))
 				return value => byte.Parse(value);
-			if (typeof(T) == typeof(sbyte) || typeof(T) == typeof(sbyte?))
+			if (typeof(T) == typeof(byte?))
+				return value => value == null ? null : (byte?) byte.Parse(value);
+			if (typeof(T) == typeof(sbyte))
 				return value => sbyte.Parse(value);
-			if (typeof(T) == typeof(short) || typeof(T) == typeof(short?))
+			if (typeof(T) == typeof(sbyte?))
+				return value => value == null ? null : (sbyte?) sbyte.Parse(value);
+			if (typeof(T) == typeof(short))
 				return value => short.Parse(value);
-			if (typeof(T) == typeof(int) || typeof(T) == typeof(int?))
+			if (typeof(T) == typeof(short?))
+				return value => value == null ? null : (short?) short.Parse(value);
+			if (typeof(T) == typeof(int))
 				return value => int.Parse(value);
-			if (typeof(T) == typeof(long) || typeof(T) == typeof(long?))
+			if (typeof(T) == typeof(int?))
+				return value => value == null ? null : (int?) int.Parse(value);
+			if (typeof(T) == typeof(long))
 				return value => long.Parse(value);
-			if (typeof(T) == typeof(float) || typeof(T) == typeof(float?))
+			if (typeof(T) == typeof(long?))
+				return value => value == null ? null : (long?) long.Parse(value);
+			if (typeof(T) == typeof(float))
 				return value => float.Parse(value, CultureInfo.InvariantCulture);
-			if (typeof(T) == typeof(double) || typeof(T) == typeof(double?))
+			if (typeof(T) == typeof(float?))
+				return value => value == null ? null : (float?) float.Parse(value, CultureInfo.InvariantCulture);
+			if (typeof(T) == typeof(double))
 				return value => double.Parse(value, CultureInfo.InvariantCulture);
-			if (typeof(T) == typeof(decimal) || typeof(T) == typeof(decimal?))
+			if (typeof(T) == typeof(double?))
+				return value => value == null ? null : (double?) double.Parse(value, CultureInfo.InvariantCulture);
+			if (typeof(T) == typeof(decimal))
 				return value => decimal.Parse(value, CultureInfo.InvariantCulture);
+			if (typeof(T) == typeof(decimal?))
+				return value => value == null ? null : (decimal?) decimal.Parse(value, CultureInfo.InvariantCulture);
 
-			if (typeof(T) == typeof(Guid) || typeof(T) == typeof(Guid?))
+			if (typeof(T) == typeof(Guid))
 				return value => new Guid(value);
-			if (typeof(T) == typeof(DateTime) || typeof(T) == typeof(DateTime?))
+			if (typeof(T) == typeof(Guid?))
+				return value => value == null ? null : (Guid?) new Guid(value);
+			if (typeof(T) == typeof(DateTime))
 				return value => DateTimeSerializer.ParseShortestXsdDateTime(value);
-			if (typeof(T) == typeof(TimeSpan) || typeof(T) == typeof(TimeSpan?))
+			if (typeof(T) == typeof(DateTime?))
+				return value => value == null ? null : (DateTime?) DateTimeSerializer.ParseShortestXsdDateTime(value);
+			if (typeof(T) == typeof(TimeSpan))
 				return value => TimeSpan.Parse(value);
+			if (typeof(T) == typeof(TimeSpan?))
+				return value => value == null ? null : (TimeSpan?) TimeSpan.Parse(value);
 
-			if (typeof(T) == typeof(char) || typeof(T) == typeof(char?))
+			if (typeof(T) == typeof(char))
 			{
 				char cValue;
 				return value => char.TryParse(value, out cValue) ? cValue : '\0';
 			}
-			if (typeof(T) == typeof(ushort) || typeof(T) == typeof(ushort?))
+			if (typeof(T) == typeof(char?))
+			{
+				char cValue;
+				return value => value == null ? null : (char?) (char.TryParse(value, out cValue) ? cValue : '\0');
+			}
+			if (typeof(T) == typeof(ushort))
 				return value => ushort.Parse(value);
-			if (typeof(T) == typeof(uint) || typeof(T) == typeof(uint?))
+			if (typeof(T) == typeof(ushort?))
+				return value => value == null ? null : (ushort?) ushort.Parse(value);
+			if (typeof(T) == typeof(uint))
 				return value => uint.Parse(value);
-			if (typeof(T) == typeof(ulong) || typeof(T) == typeof(ulong?))
+			if (typeof(T) == typeof(uint?))
+				return value => value == null ? null : (uint?) uint.Parse(value);
+			if (typeof(T) == typeof(ulong))
 				return value => ulong.Parse(value);
+			if (typeof(T) == typeof(ulong?))
+				return value => value == null ? null : (ulong?) ulong.Parse(value);
 
 			return null;
 		}
