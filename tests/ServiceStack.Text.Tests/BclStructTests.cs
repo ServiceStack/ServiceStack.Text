bce86146dc622fb3a5e7511e8bf02963b30eade5
Demis Bellot <demis.bellot@gmail.com>
2011-10-30 12:54:15 -0400

    Added test for reported issue #21 for testing serializing arrays of enums.

--- tests/ServiceStack.Text.Tests/BclStructTests.cs
+++ tests/ServiceStack.Text.Tests/BclStructTests.cs
@@ -15,5 +15,23 @@ namespace ServiceStack.Text.Tests
 
 			Assert.That(fromColor, Is.EqualTo(color));
 		}
+
+		public enum MyEnum
+		{
+			Enum1,
+			Enum2,
+			Enum3,
+		}
+
+		[Test]
+		public void Can_serialize_arrays_of_enums()
+		{
+			var enums = new[] { MyEnum.Enum1, MyEnum.Enum2, MyEnum.Enum3 };
+			var fromEnums = Serialize(enums);
+
+			Assert.That(fromEnums[0], Is.EqualTo(MyEnum.Enum1));
+			Assert.That(fromEnums[1], Is.EqualTo(MyEnum.Enum2));
+			Assert.That(fromEnums[2], Is.EqualTo(MyEnum.Enum3));
+		}
 	}
 }
