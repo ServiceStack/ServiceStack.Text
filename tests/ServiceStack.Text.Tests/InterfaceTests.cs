5b597345eaf40d8916dbc1f916baa35aca08b050
mythz <demis.bellot@gmail.com>
2011-11-08 18:50:30 -0500

    fix build error

--- tests/ServiceStack.Text.Tests/InterfaceTests.cs
+++ tests/ServiceStack.Text.Tests/InterfaceTests.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using ServiceStack.Messaging;
 using ServiceStack.ServiceHost;
-using ServiceStack.ServiceInterface.OAuth;
+using ServiceStack.ServiceInterface.Auth;
 using ServiceStack.ServiceInterface.ServiceModel;
 using ServiceStack.Text.Tests.JsonTests;
 
