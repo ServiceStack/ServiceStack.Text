ba3fbcaaca4187168600c259dc84c8422188b711
Aurimas86 <neverauskas@gmail.com>
2012-02-29 19:34:20 +0200

    Update src/ServiceStack.Text/Json/JsonTypeSerializer.cs

--- src/ServiceStack.Text/Json/JsonTypeSerializer.cs
+++ src/ServiceStack.Text/Json/JsonTypeSerializer.cs
@@ -255,7 +255,7 @@ namespace ServiceStack.Text.Json
 
 		public void WriteLinqBinary(TextWriter writer, object linqBinaryValue)
 		{
-#if !MONOTOUCH
+#if !MONOTOUCH && !SILVERLIGHT
 			WriteRawString(writer, Convert.ToBase64String(((System.Data.Linq.Binary)linqBinaryValue).ToArray()));
 #endif
 		}
