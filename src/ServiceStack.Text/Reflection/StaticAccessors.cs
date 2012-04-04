b360607903cdcb1749cf251cdf4eafcbcf25ec86
Mijail Cisneros <m@spbtv.com>
2012-04-04 14:27:03 +0400

    Improved Performance by adding Chris Sainty Linq.Expressions fixes. https://github.com/csainty/ServiceStack.Text/commit/32112d9e966f15a4a6d3b43e68441ff765bde2e0

--- src/ServiceStack.Text/Reflection/StaticAccessors.cs
+++ src/ServiceStack.Text/Reflection/StaticAccessors.cs
@@ -13,64 +13,64 @@ using System;
 using System.Reflection;
 
 #if !XBOX
-using System.Linq.Expressions ;
+using System.Linq.Expressions;
 #endif
 namespace ServiceStack.Text.Reflection
 {
-	public static class StaticAccessors
-	{
-		public static Func<object, object> GetValueGetter(this PropertyInfo propertyInfo, Type type)
-		{
-#if SILVERLIGHT || MONOTOUCH || XBOX
+    public static class StaticAccessors
+    {
+        public static Func<object, object> GetValueGetter(this PropertyInfo propertyInfo, Type type)
+        {
+#if (SILVERLIGHT && !WINDOWS_PHONE) || MONOTOUCH || XBOX
 			var getMethodInfo = propertyInfo.GetGetMethod();
 			if (getMethodInfo == null) return null;
 			return x => getMethodInfo.Invoke(x, new object[0]);
 #else
 
-			var instance = Expression.Parameter(typeof(object), "i");
-			var convertInstance = Expression.TypeAs(instance, propertyInfo.DeclaringType);
-			var property = Expression.Property(convertInstance, propertyInfo);
-			var convertProperty = Expression.TypeAs(property, typeof(object));
-			return Expression.Lambda<Func<object, object>>(convertProperty, instance).Compile();
+            var instance = Expression.Parameter(typeof(object), "i");
+            var convertInstance = Expression.TypeAs(instance, propertyInfo.DeclaringType);
+            var property = Expression.Property(convertInstance, propertyInfo);
+            var convertProperty = Expression.TypeAs(property, typeof(object));
+            return Expression.Lambda<Func<object, object>>(convertProperty, instance).Compile();
 #endif
-		}
+        }
 
-		public static Func<T, object> GetValueGetter<T>(this PropertyInfo propertyInfo)
-		{
-#if SILVERLIGHT || MONOTOUCH || XBOX
+        public static Func<T, object> GetValueGetter<T>(this PropertyInfo propertyInfo)
+        {
+#if (SILVERLIGHT && !WINDOWS_PHONE) || MONOTOUCH || XBOX
 			var getMethodInfo = propertyInfo.GetGetMethod();
 			if (getMethodInfo == null) return null;
 			return x => getMethodInfo.Invoke(x, new object[0]);
 #else
-			var instance = Expression.Parameter(propertyInfo.DeclaringType, "i");
-			var property = Expression.Property(instance, propertyInfo);
-			var convert = Expression.TypeAs(property, typeof(object));
-			return Expression.Lambda<Func<T, object>>(convert, instance).Compile();
+            var instance = Expression.Parameter(propertyInfo.DeclaringType, "i");
+            var property = Expression.Property(instance, propertyInfo);
+            var convert = Expression.TypeAs(property, typeof(object));
+            return Expression.Lambda<Func<T, object>>(convert, instance).Compile();
 #endif
-		}
+        }
 
 #if !XBOX
-		public static Action<T, object> GetValueSetter<T>(this PropertyInfo propertyInfo)
-		{
-			if (typeof(T) != propertyInfo.DeclaringType)
-			{
-				throw new ArgumentException();
-			}
+        public static Action<T, object> GetValueSetter<T>(this PropertyInfo propertyInfo)
+        {
+            if (typeof(T) != propertyInfo.DeclaringType)
+            {
+                throw new ArgumentException();
+            }
 
-			var instance = Expression.Parameter(propertyInfo.DeclaringType, "i");
-			var argument = Expression.Parameter(typeof(object), "a");
-			var setterCall = Expression.Call(
-				instance,
-				propertyInfo.GetSetMethod(),
-				Expression.Convert(argument, propertyInfo.PropertyType));
+            var instance = Expression.Parameter(propertyInfo.DeclaringType, "i");
+            var argument = Expression.Parameter(typeof(object), "a");
+            var setterCall = Expression.Call(
+                instance,
+                propertyInfo.GetSetMethod(),
+                Expression.Convert(argument, propertyInfo.PropertyType));
 
-			return Expression.Lambda<Action<T, object>>
-			(
-				setterCall, instance, argument
-			).Compile();
-		}
+            return Expression.Lambda<Action<T, object>>
+            (
+                setterCall, instance, argument
+            ).Compile();
+        }
 #endif
 
-	}
+    }
 }
 
