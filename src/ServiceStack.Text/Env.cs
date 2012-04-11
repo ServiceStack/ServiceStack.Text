260d0f36b10f2756740269ce913aff63c7848879
mythz <demis.bellot@gmail.com>
2012-04-11 15:03:10 -0400

    incr to v3.68

--- src/ServiceStack.Text/Env.cs
+++ src/ServiceStack.Text/Env.cs
@@ -23,7 +23,7 @@ namespace ServiceStack.Text
 				+ (IsMonoTouch ? " MonoTouch" : "");
 		}
 
-		public static decimal ServiceStackVersion = 3.67m;
+		public static decimal ServiceStackVersion = 3.68m;
 
 		public static bool IsUnix { get; set; }
 
