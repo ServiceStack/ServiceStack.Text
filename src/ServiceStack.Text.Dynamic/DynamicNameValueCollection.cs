c0aa281f1721c3c835619eee9d1c79c4db185f8d
Jon Canning <jon.canning@gmail.com>
2012-06-17 20:12:31 +0100

    Tidied up constructor

--- src/ServiceStack.Text.Dynamic/DynamicNameValueCollection.cs
+++ src/ServiceStack.Text.Dynamic/DynamicNameValueCollection.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
 
@@ -6,6 +5,6 @@ namespace ServiceStack.Text.Dynamic
 {
     public class DynamicNameValueCollection : DynamicDictionary
     {
-        public DynamicNameValueCollection(NameValueCollection nameValueCollection) : base(nameValueCollection.AllKeys.Aggregate(new Dictionary<string, string>(), (d, s) => { d.Add(s, nameValueCollection[s]); return d; })) { }
+        public DynamicNameValueCollection(NameValueCollection nameValueCollection) : base(nameValueCollection.AllKeys.ToDictionary(x => x, x => nameValueCollection[x])) { }
     }
 }
