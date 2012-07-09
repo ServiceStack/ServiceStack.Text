b01d8100410a56d4766d8612b08c8b1de63e3560
mythz <demis.bellot@gmail.com>
2012-07-09 18:25:52 -0400

    INCR Env to v3.85

--- src/ServiceStack.Text/Env.cs
+++ src/ServiceStack.Text/Env.cs
@@ -23,7 +23,7 @@ namespace ServiceStack.Text
 				+ (IsMonoTouch ? " MonoTouch" : "");
 		}
 
-		public static decimal ServiceStackVersion = 3.83m;
+		public static decimal ServiceStackVersion = 3.85m;
 
 		public static bool IsUnix { get; set; }
 
