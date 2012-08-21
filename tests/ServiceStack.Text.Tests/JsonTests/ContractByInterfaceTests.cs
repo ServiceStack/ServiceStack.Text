5a8e3f8c58e1a00467b795582abd900135fb564e
Iain Ballard <ieb@me.com>
2012-08-21 22:05:40 +0100

    Testing dynamic proxy inside dynamic proxy

--- tests/ServiceStack.Text.Tests/JsonTests/ContractByInterfaceTests.cs
+++ tests/ServiceStack.Text.Tests/JsonTests/ContractByInterfaceTests.cs
@@ -11,7 +11,7 @@ namespace ServiceStack.Text.Tests.JsonTests {
 		[Test]
 		public void Should_be_able_to_serialise_based_on_an_interface () {
 			JsConfig.PreferInterfaces = true;
-			IContract myConcrete = new Concrete("boo");
+			IContract myConcrete = new Concrete("boo", 1);
 			var json = JsonSerializer.SerializeToString(myConcrete, typeof(IContract));
 
 			Console.WriteLine(json);
@@ -21,7 +21,7 @@ namespace ServiceStack.Text.Tests.JsonTests {
 		[Test]
 		public void Should_not_use_interface_type_if_concrete_specified () {
 			JsConfig.PreferInterfaces = false;
-			IContract myConcrete = new Concrete("boo");
+			IContract myConcrete = new Concrete("boo", 1);
 			var json = JsonSerializer.SerializeToString(myConcrete, typeof(IContract));
 
 			Console.WriteLine(json);
@@ -31,27 +31,40 @@ namespace ServiceStack.Text.Tests.JsonTests {
 		[Test]
 		public void Should_be_able_to_deserialise_based_on_an_interface_with_no_concrete () {
 			JsConfig.PreferInterfaces = true;
-			var json = JsonSerializer.SerializeToString(new Concrete("boo"), typeof(IContract));
+			var json = JsonSerializer.SerializeToString(new Concrete("boo", 42), typeof(IContract));
 			json = json.Replace("ServiceStack.Text.Tests.JsonTests.IContract", "ServiceStack.Text.Tests.JsonTests.IIdenticalContract");
 
+			Console.WriteLine(json);
 			var result = JsonSerializer.DeserializeFromString<IIdenticalContract>(json);
 
 			Assert.That(result.StringValue, Is.EqualTo("boo"));
+			Assert.That(result.ChildProp.IntValue, Is.EqualTo(42));
 		}
 	}
 
 	public class Concrete : IContract {
-		public Concrete(string boo) {
+		public Concrete(string boo, int i) {
 			StringValue = boo;
+			ChildProp = new ConcreteChild { IntValue = i };
 		}
 
 		public string StringValue { get; set; }
+		public IChildInterface ChildProp { get; set; }
+	}
+	public class ConcreteChild : IChildInterface {
+		public int IntValue { get; set; }
+	}
+
+	public interface IChildInterface {
+		int IntValue { get; set; }
 	}
 
 	public interface IContract {
 		string StringValue { get; set; }
+		IChildInterface ChildProp { get; set; }
 	}
 	public interface IIdenticalContract {
 		string StringValue { get; set; }
+		IChildInterface ChildProp { get; set; }
 	}
 }
