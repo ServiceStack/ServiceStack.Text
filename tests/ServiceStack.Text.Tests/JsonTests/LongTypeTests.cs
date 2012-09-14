3792b033145762ed514d0a350f3fa8e5a152b38c
Iain Ballard <iain.ballard@7digital.com>
2012-09-14 10:20:32 +0100

    Failing test for Get-only field backed properties

---
+++ tests/ServiceStack.Text.Tests/JsonTests/LongTypeTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ServiceStack.Text.Tests.JsonTests
+{
+
+	#region Test types
+
+	public class Timestamp
+	{
+		long x;
+
+		public Timestamp(long i)
+		{
+			x = i;
+		}
+
+		public long UnixTime { get { return x; }
+		}
+	}
+
+	#endregion
+
+	[TestFixture]
+	public class LongTypeTests
+	{
+		[Test]
+		public void Backed_get_properties_can_be_deserialised()
+		{
+			var original = new Timestamp(123344044);
+			var str1 = original.ToJson();
+			var copy = str1.FromJson<Timestamp>();
+
+			Console.WriteLine(str1);
+
+			Assert.That(copy.UnixTime, Is.EqualTo(original.UnixTime));
+		}
+	}
+}
