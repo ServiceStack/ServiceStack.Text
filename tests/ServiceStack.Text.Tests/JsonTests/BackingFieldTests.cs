0e83165be3221d2273b313ce38ffa09fe30179c9
Demis Bellot <demis.bellot@gmail.com>
2012-09-15 16:16:36 -0400

    fix comment + explain behavior

--- tests/ServiceStack.Text.Tests/JsonTests/BackingFieldTests.cs
+++ tests/ServiceStack.Text.Tests/JsonTests/BackingFieldTests.cs
@@ -64,8 +64,9 @@ namespace ServiceStack.Text.Tests.JsonTests
 			Console.WriteLine(str1);
 
 			// ReflectionExtensions.cs Line 417 is being used to determine *deserialisable*
-			// for properties type based on if the property is *readable*, not *writable*
-			// TODO: Fix this!
+			// for properties type based on if the property is *readable*, not *writable* -- by design
+
+            //Rule: To be emitted properties should be readable, to be deserialized properties should be writeable
 
 			Assert.That(copy.Property, Is.EqualTo(original.Property));
 		}
