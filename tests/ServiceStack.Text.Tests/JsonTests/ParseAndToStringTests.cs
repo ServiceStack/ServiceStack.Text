781be4bb8d91647e4afd34775e0b06f60af20bda
Iain Ballard <ieb@me.com>
2012-09-18 22:40:52 +0100

    Covering parsing for future changes

---
+++ tests/ServiceStack.Text.Tests/JsonTests/ParseAndToStringTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ServiceStack.Text.Tests.JsonTests {
+	#region Test types
+	public struct ToStringOnly {
+		public string Value { get; set; }
+		public override string ToString () {
+			return "WRONG!";
+		}
+	}
+
+	public struct ToStringAndParse {
+		public string Value { get; set; }
+		public override string ToString () {
+			return "OK";
+		}
+		public static ToStringAndParse Parse (string value) {
+			return new ToStringAndParse { Value = "OK" };
+		}
+	}
+	#endregion
+	[TestFixture]
+	public class ParseAndToStringTests {
+		[Test]
+		public void Should_use_ToString_if_type_has_parse_method () {
+			var original = new ToStringAndParse{Value="WRONG!"};
+			var str = original.ToJson();
+			var copy = str.FromJson<ToStringAndParse>();
+
+			Console.WriteLine(str);
+			Assert.That(copy.Value, Is.EqualTo("OK"));
+		}
+
+		[Test]
+		public void Should_not_use_ToString_if_type_has_no_parse_method () {
+			var original = new ToStringAndParse{Value="OK"};
+			var str = original.ToJson();
+			var copy = str.FromJson<ToStringAndParse>();
+
+			Console.WriteLine(str);
+			Assert.That(copy.Value, Is.EqualTo("OK"));
+		}
+	}
+}
