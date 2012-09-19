bb5b293786296b6886f447019a51141a525eaf6f
Iain Ballard <iain.ballard@7digital.com>
2012-09-19 12:58:21 +0100

    Fixed tostring/parse test, is now red (correctly)

--- tests/ServiceStack.Text.Tests/JsonTests/ParseAndToStringTests.cs
+++ tests/ServiceStack.Text.Tests/JsonTests/ParseAndToStringTests.cs
@@ -5,6 +5,7 @@ namespace ServiceStack.Text.Tests.JsonTests {
 	#region Test types
 	public struct ToStringOnly {
 		public string Value { get; set; }
+
 		public override string ToString () {
 			return "WRONG!";
 		}
@@ -19,12 +20,18 @@ namespace ServiceStack.Text.Tests.JsonTests {
 			return new ToStringAndParse { Value = "OK" };
 		}
 	}
+
+	public class Container
+	{
+		public object Contents { get; set; }
+	}
+
 	#endregion
 	[TestFixture]
 	public class ParseAndToStringTests {
 		[Test]
 		public void Should_use_ToString_if_type_has_parse_method () {
-			var original = new ToStringAndParse{Value="WRONG!"};
+			var original = new Container{Contents = new ToStringAndParse{Value="WRONG!"}};
 			var str = original.ToJson();
 			var copy = str.FromJson<ToStringAndParse>();
 
@@ -34,9 +41,9 @@ namespace ServiceStack.Text.Tests.JsonTests {
 
 		[Test]
 		public void Should_not_use_ToString_if_type_has_no_parse_method () {
-			var original = new ToStringAndParse{Value="OK"};
+			var original = new Container{Contents = new ToStringOnly{Value="OK"}};
 			var str = original.ToJson();
-			var copy = str.FromJson<ToStringAndParse>();
+			var copy = str.FromJson<ToStringOnly>();
 
 			Console.WriteLine(str);
 			Assert.That(copy.Value, Is.EqualTo("OK"));
