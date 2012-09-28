fe45dc655e4d91834e85d2c325c12a0e16690f57
Sergey Bogdanov <sergey.bogdanov@gmail.com>
2012-09-28 22:07:19 +0300

    fixed #206: - extended ToTileCase to support SL5 - added more tests for StringExtensions

--- src/ServiceStack.Text/Common/DeserializeTypeRefJson.cs
+++ src/ServiceStack.Text/Common/DeserializeTypeRefJson.cs
@@ -42,28 +42,6 @@ namespace ServiceStack.Text.Common
 
     }
 
-#if SILVERLIGHT
-    public static class TextInfoSilverLightExtension
-    {
-        public static string ToTitleCase(this TextInfo textInfo, string str)
-        {
-            string[] words = str.Split('_');
-
-            for (int i = 0; i <= words.Length - 1; i++)
-            {
-                if ((!object.ReferenceEquals(words[i], string.Empty)))
-                {
-                    string firstLetter = words[i].Substring(0, 1);
-                    string rest = words[i].Substring(1);
-                    string result = firstLetter.ToUpper() + rest.ToLower();
-                    words[i] = result;
-                }
-            }
-            return String.Join("_", words);
-        }
-    }
-#endif
-
 	internal static class DeserializeTypeRefJson
 	{
 	    public static readonly IPropertyNameResolver DefaultPropertyNameResolver = new DefaultPropertyNameResolver();
