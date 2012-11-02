fbe15427ae0f79085767cb56f92516da8ac0c29d
Demis Bellot <demis.bellot@gmail.com>
2012-11-02 19:57:50 -0400

    Add example of detecting DTO's with no version number

--- tests/ServiceStack.Text.Tests/JsonTests/CustomSerializerTests.cs
+++ tests/ServiceStack.Text.Tests/JsonTests/CustomSerializerTests.cs
@@ -6,33 +6,33 @@ using System.Runtime.Serialization;
 
 namespace ServiceStack.Text.Tests.JsonTests
 {
-	public class CustomSerializerTests : TestBase
-	{
-		static CustomSerializerTests()
-		{
+    public class CustomSerializerTests : TestBase
+    {
+        static CustomSerializerTests()
+        {
             JsConfig<EntityWithValues>.RawSerializeFn = SerializeEntity;
             JsConfig<EntityWithValues>.RawDeserializeFn = DeserializeEntity;
-		}
-        
+        }
+
         [TestFixtureTearDown]
         public void TestFixtureTearDown()
         {
             JsConfig.Reset();
         }
 
-		[Test]
-		public void Can_serialize_Entity()
-		{
-		    var originalEntity = new EntityWithValues {id = 5, Values = new Dictionary<string, string> {{"dog", "bark"}, {"cat", "meow"}}};
-		    JsonSerializeAndCompare(originalEntity);
-		}
+        [Test]
+        public void Can_serialize_Entity()
+        {
+            var originalEntity = new EntityWithValues { id = 5, Values = new Dictionary<string, string> { { "dog", "bark" }, { "cat", "meow" } } };
+            JsonSerializeAndCompare(originalEntity);
+        }
 
-		[Test]
-		public void Can_serialize_arrays_of_entities()
-		{
-			var originalEntities = new[] { new EntityWithValues {id = 5, Values = new Dictionary<string, string> {{"dog", "bark"}}}, new EntityWithValues {id = 6, Values = new Dictionary<string, string> {{"cat", "meow"}}} };
-			JsonSerializeAndCompare(originalEntities);
-		}
+        [Test]
+        public void Can_serialize_arrays_of_entities()
+        {
+            var originalEntities = new[] { new EntityWithValues { id = 5, Values = new Dictionary<string, string> { { "dog", "bark" } } }, new EntityWithValues { id = 6, Values = new Dictionary<string, string> { { "cat", "meow" } } } };
+            JsonSerializeAndCompare(originalEntities);
+        }
 
         public class EntityWithValues
         {
@@ -73,7 +73,8 @@ namespace ServiceStack.Text.Tests.JsonTests
         private static string SerializeEntity(EntityWithValues entity)
         {
             var dictionary = entity.Values.ToDictionary(pair => pair.Key, pair => pair.Value);
-            if (entity.id > 0) {
+            if (entity.id > 0)
+            {
                 dictionary["id"] = entity.id.ToString(CultureInfo.InvariantCulture);
             }
             return JsonSerializer.SerializeToString(dictionary);
@@ -84,71 +85,125 @@ namespace ServiceStack.Text.Tests.JsonTests
             var dictionary = JsonSerializer.DeserializeFromString<Dictionary<string, string>>(value);
             if (dictionary == null) return null;
             var entity = new EntityWithValues();
-            foreach (var pair in dictionary) {
-                if (pair.Key == "id") {
-                    if (!string.IsNullOrEmpty(pair.Value)) {
+            foreach (var pair in dictionary)
+            {
+                if (pair.Key == "id")
+                {
+                    if (!string.IsNullOrEmpty(pair.Value))
+                    {
                         entity.id = int.Parse(pair.Value);
                     }
-                } else {
+                }
+                else
+                {
                     entity.Values.Add(pair.Key, pair.Value);
                 }
             }
             return entity;
         }
 
-		[DataContract]
-		private class Test1Base
-		{
-			public Test1Base(bool itb, bool itbm)
-			{
-				InTest1Base = itb; InTest1BaseM = itbm;
-			}
+        [DataContract]
+        private class Test1Base
+        {
+            public Test1Base(bool itb, bool itbm)
+            {
+                InTest1Base = itb; InTest1BaseM = itbm;
+            }
 
-			[DataMember]
-			public bool InTest1BaseM { get; set; }
+            [DataMember]
+            public bool InTest1BaseM { get; set; }
 
-			public bool InTest1Base { get; set; }
-		}
+            public bool InTest1Base { get; set; }
+        }
 
-		[DataContract]
-		private class Test1 : Test1Base
-		{
-			public Test1(bool it, bool itm, bool itb, bool itbm):base(itb, itbm)
-			{
-				InTest1 = it; InTest1M = itm;
-			}
+        [DataContract]
+        private class Test1 : Test1Base
+        {
+            public Test1(bool it, bool itm, bool itb, bool itbm)
+                : base(itb, itbm)
+            {
+                InTest1 = it; InTest1M = itm;
+            }
 
-			[DataMember]
-			public bool InTest1M { get; set; }
+            [DataMember]
+            public bool InTest1M { get; set; }
 
-			public bool InTest1 { get; set; }
-		}
+            public bool InTest1 { get; set; }
+        }
 
-		[Test]
-		public void Can_Serialize_With_Custom_Constructor()
-		{
-			bool hit = false;
-			JsConfig.ModelFactory = type =>
-			{
-				if (typeof(Test1) == type)
-				{
-					hit = true;
-					return () => new Test1(false, false, true, false);
-				}
-				return null;
-			};
+        [Test]
+        public void Can_Serialize_With_Custom_Constructor()
+        {
+            bool hit = false;
+            JsConfig.ModelFactory = type => {
+                if (typeof(Test1) == type)
+                {
+                    hit = true;
+                    return () => new Test1(false, false, true, false);
+                }
+                return null;
+            };
 
-			var t1 = new Test1(true, true, true, true);
+            var t1 = new Test1(true, true, true, true);
 
-			var data = JsonSerializer.SerializeToString(t1);
+            var data = JsonSerializer.SerializeToString(t1);
 
-			var t2 = JsonSerializer.DeserializeFromString<Test1>(data);
+            var t2 = JsonSerializer.DeserializeFromString<Test1>(data);
 
-			Assert.IsTrue(hit);
-			Assert.IsTrue(t2.InTest1BaseM);
-			Assert.IsTrue(t2.InTest1M);
-			Assert.IsTrue(t2.InTest1Base);
-			Assert.IsFalse(t2.InTest1);
-		}
-	}
+            Assert.IsTrue(hit);
+            Assert.IsTrue(t2.InTest1BaseM);
+            Assert.IsTrue(t2.InTest1M);
+            Assert.IsTrue(t2.InTest1Base);
+            Assert.IsFalse(t2.InTest1);
+        }
+
+
+        public class Dto
+        {
+            public string Name { get; set; }
+        }
+
+        public interface IHasVersion
+        {
+            int Version { get; set; }
+        }
+
+        public class DtoV1 : IHasVersion
+        {
+            public int Version { get; set; }
+            public string Name { get; set; }
+
+            public DtoV1()
+            {
+                Version = 1;
+            }
+        }
+
+        [Test]
+        public void Can_detect_dto_with_no_Version()
+        {
+            JsConfig.ModelFactory = type => {
+                if (typeof(IHasVersion).IsAssignableFrom(type))
+                {
+                    return () => {
+                        var obj = (IHasVersion)type.CreateInstance();
+                        obj.Version = 0;
+                        return obj;
+                    };
+                }
+                return () => type.CreateInstance();
+            };
+
+            var dto = new Dto { Name = "Foo" };
+            var fromDto = dto.ToJson().FromJson<DtoV1>();
+            Assert.That(fromDto.Version, Is.EqualTo(0));
+            Assert.That(fromDto.Name, Is.EqualTo("Foo"));
+
+            var dto1 = new DtoV1 { Name = "Foo 1" };
+            var fromDto1 = dto1.ToJson().FromJson<DtoV1>();
+            Assert.That(fromDto1.Version, Is.EqualTo(1));
+            Assert.That(fromDto1.Name, Is.EqualTo("Foo 1"));
+        }
+
+    }
 }
