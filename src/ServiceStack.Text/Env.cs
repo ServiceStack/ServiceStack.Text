654bbf18f93213aa6be039dab3b2932be2eeebc2
Demis Bellot <demis.bellot@gmail.com>
2013-02-18 05:40:13 -0500

    INCR to v3.9.37

--- src/ServiceStack.Text/Env.cs
+++ src/ServiceStack.Text/Env.cs
@@ -23,7 +23,7 @@ namespace ServiceStack.Text
 				+ (IsMonoTouch ? " MonoTouch" : "");
 		}
 
-		public static decimal ServiceStackVersion = 3.935m;
+		public static decimal ServiceStackVersion = 3.937m;
 
 		public static bool IsUnix { get; set; }
 
