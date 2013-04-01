8af627b614a488b9a469269e21ff5f583cc63bfe
Demis Bellot <demis.bellot@gmail.com>
2013-04-01 04:24:29 -0400

    INCR to v3.9.43

--- src/ServiceStack.Text/Env.cs
+++ src/ServiceStack.Text/Env.cs
@@ -34,7 +34,7 @@ namespace ServiceStack.Text
                 + (IsWinRT ? ".NET WinRT" : "");
 		}
 
-		public static decimal ServiceStackVersion = 3.942m;
+		public static decimal ServiceStackVersion = 3.943m;
 
 		public static bool IsUnix { get; set; }
 
