7c6405952edfe132df0c435e27537c693137f65e
Demis Bellot <demis.bellot@gmail.com>
2013-06-10 03:10:28 -0400

    INCR to vNext v3.9.50

--- src/ServiceStack.Text/Env.cs
+++ src/ServiceStack.Text/Env.cs
@@ -34,7 +34,7 @@ namespace ServiceStack.Text
                 + (IsWinRT ? ".NET WinRT" : "");
 		}
 
-		public static decimal ServiceStackVersion = 3.949m;
+		public static decimal ServiceStackVersion = 3.950m;
 
 		public static bool IsUnix { get; set; }
 
