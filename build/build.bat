86c248fa8c31b7deddfc997e15ececa4bb2b0fcf
Demis Bellot <demis.bellot@gmail.com>
2013-09-13 16:16:31 -0400

    Update Build.proj add SL5 builds, symbols, overridable PatchVersion

--- build/build.bat
+++ build/build.bat
@@ -11,5 +11,7 @@ COPY ..\src\ServiceStack.Text\bin\%BUILD%\*.* ..\..\ServiceStack.OrmLite\lib
 COPY ..\src\ServiceStack.Text\bin\%BUILD%\*.* ..\..\ServiceStack.Examples\lib
 COPY ..\src\ServiceStack.Text\bin\%BUILD%\*.* ..\..\ServiceStack.RedisWebServices\lib
 
+COPY ..\src\ServiceStack.Text.SL5\bin\%BUILD%\*.* ..\..\ServiceStack\lib\sl5
+
 MD ..\NuGet\lib\net35
 COPY ..\src\ServiceStack.Text\bin\%BUILD%\*.* ..\NuGet\lib\net35
