b88fd273369a8bc5e9cd537bb80a071318a4eef4
James Cunningham <james@einherj.ar.com>
2013-10-04 06:55:21 +0000

    Fixes Should_not_put_double_underscore. Added Should_not_split_digits.

--- src/ServiceStack.Text/StringExtensions.cs
+++ src/ServiceStack.Text/StringExtensions.cs
@@ -692,7 +692,7 @@ namespace ServiceStack.Text
             var sb = new StringBuilder(value.Length);
             foreach (var t in value)
             {
-                if (Char.IsLower(t) || t == '_')
+                if (Char.IsDigit(t) || (Char.IsLetter(t) && Char.IsLower(t)) || t == '_')
                 {
                     sb.Append(t);
                 }
