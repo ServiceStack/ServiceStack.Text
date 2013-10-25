61ccf98ec2fabcebeb9df080d659749daddfd2a0
Demis Bellot <demis.bellot@gmail.com>
2013-10-25 01:29:25 -0400

    Change copy.bat to copy over signed dlls as well

--- build/copy.bat
+++ build/copy.bat
@@ -7,6 +7,10 @@ COPY ..\src\ServiceStack.Text\bin\%BUILD%\*.* ..\..\ServiceStack.Contrib\lib
 COPY ..\src\ServiceStack.Text\bin\%BUILD%\*.* ..\..\ServiceStack.Redis\lib
 COPY ..\src\ServiceStack.Text\bin\%BUILD%\*.* ..\..\ServiceStack.OrmLite\lib
 
+COPY ..\src\ServiceStack.Text\bin\Signed\*.* ..\..\ServiceStack\lib\signed
+COPY ..\src\ServiceStack.Text\bin\Signed\*.* ..\..\ServiceStack.Redis\lib\signed
+COPY ..\src\ServiceStack.Text\bin\Signed\*.* ..\..\ServiceStack.OrmLite\lib\signed
+
 COPY ..\src\ServiceStack.Text.SL5\bin\%BUILD%\*.* ..\..\ServiceStack\lib\sl5
 
 MD ..\NuGet\lib\net35
