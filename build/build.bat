2191c252922afcb70505ac8545b486fc3912e6e5
Demis Bellot <demis.bellot@gmail.com>
2013-10-25 01:26:52 -0400

    Add build-sn.proj

--- build/build.bat
+++ build/build.bat
@@ -1,3 +1,4 @@
 SET MSBUILD=C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
 
-%MSBUILD% Build.proj /target:NuGetPack /property:Configuration=Release;RELEASE=true;PatchVersion=1
+REM %MSBUILD% build.proj /target:TeamCityBuild /property:Configuration=Release;RELEASE=true;PatchVersion=1
+%MSBUILD% build-sn.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Signed;RELEASE=true;PatchVersion=1
