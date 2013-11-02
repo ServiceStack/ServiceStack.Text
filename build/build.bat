2d43d8a81e787dd2ae8ef0798765d66341b53ec1
Demis Bellot <demis.bellot@gmail.com>
2013-11-01 20:43:34 -0400

    Fix SS.Text build

--- build/build.bat
+++ build/build.bat
@@ -1,4 +1,4 @@
 SET MSBUILD=C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
 
-REM %MSBUILD% build.proj /target:TeamCityBuild /property:Configuration=Release;RELEASE=true;PatchVersion=1
-%MSBUILD% build-sn.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Signed;RELEASE=true;PatchVersion=1
+%MSBUILD% build.proj /target:UpdateVersion /property:Configuration=Release;RELEASE=true;PatchVersion=9
+REM %MSBUILD% build-sn.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Signed;RELEASE=true;PatchVersion=1
