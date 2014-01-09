d06b08b34155ff48a582b1c9ebff954d979d9a5c
Demis Bellot <demis.bellot@gmail.com>
2014-01-09 17:21:21 +0800

    Use license in appSettings for non SL5 tests

--- tests/ServiceStack.Text.Tests/LicensingTests.cs
+++ tests/ServiceStack.Text.Tests/LicensingTests.cs
@@ -88,8 +88,11 @@ namespace ServiceStack.Text.Tests
         [Test]
         public void Can_register_valid_license()
         {
-            //Licensing.RegisterLicense(new AppSettings().GetString("servicestack:license"));
+#if !SL5
+            Licensing.RegisterLicense(new ServiceStack.Configuration.AppSettings().GetString("servicestack:license"));
+#else
             Licensing.RegisterLicense("1001-e1JlZjoxMDAxLE5hbWU6VGVzdCBCdXNpbmVzcyxUeXBlOkJ1c2luZXNzLEhhc2g6UHVNTVRPclhvT2ZIbjQ5MG5LZE1mUTd5RUMzQnBucTFEbTE3TDczVEF4QUNMT1FhNXJMOWkzVjFGL2ZkVTE3Q2pDNENqTkQyUktRWmhvUVBhYTBiekJGUUZ3ZE5aZHFDYm9hL3lydGlwUHI5K1JsaTBYbzNsUC85cjVJNHE5QVhldDN6QkE4aTlvdldrdTgyTk1relY2eis2dFFqTThYN2lmc0JveHgycFdjPSxFeHBpcnk6MjAxMy0wMS0wMX0=");
+#endif
             Assert.That(LicenseUtils.ActivatedLicenseFeatures(), Is.EqualTo(LicenseFeature.Business));
         }
 
