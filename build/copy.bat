a6ef1e15d9ae81cef582c55e8d893c120f51b7ff
Demis Bellot <demis.bellot@gmail.com>
2014-03-01 19:34:18 -0500

    Don't override other projs with dirty dlls

--- build/copy.bat
+++ build/copy.bat
@@ -2,18 +2,15 @@
 REM SET BUILD=Debug
 SET BUILD=Release
 
-COPY ..\src\ServiceStack.Text\bin\%BUILD%\*.* ..\..\ServiceStack\lib
-COPY ..\src\ServiceStack.Text\bin\%BUILD%\*.* ..\..\ServiceStack.Contrib\lib
-COPY ..\src\ServiceStack.Text\bin\%BUILD%\*.* ..\..\ServiceStack.Redis\lib
-COPY ..\src\ServiceStack.Text\bin\%BUILD%\*.* ..\..\ServiceStack.OrmLite\lib
+COPY ..\src\ServiceStack.Text\bin\%BUILD%\ServiceStack.Text.* ..\..\ServiceStack\lib
+COPY ..\src\ServiceStack.Text\bin\%BUILD%\ServiceStack.Text.* ..\..\ServiceStack.Contrib\lib
+COPY ..\src\ServiceStack.Text\bin\%BUILD%\ServiceStack.Text.* ..\..\ServiceStack.Redis\lib
+COPY ..\src\ServiceStack.Text\bin\%BUILD%\ServiceStack.Text.* ..\..\ServiceStack.OrmLite\lib
 
-COPY ..\src\ServiceStack.Text\bin\Signed\*.* ..\..\ServiceStack\lib\signed
-COPY ..\src\ServiceStack.Text\bin\Signed\*.* ..\..\ServiceStack.Redis\lib\signed
-COPY ..\src\ServiceStack.Text\bin\Signed\*.* ..\..\ServiceStack.OrmLite\lib\signed
+COPY ..\src\ServiceStack.Text\bin\Signed\ServiceStack.Text.* ..\..\ServiceStack\lib\signed
+COPY ..\src\ServiceStack.Text\bin\Signed\ServiceStack.Text.* ..\..\ServiceStack.Redis\lib\signed
+COPY ..\src\ServiceStack.Text\bin\Signed\ServiceStack.Text.* ..\..\ServiceStack.OrmLite\lib\signed
 
-COPY ..\src\ServiceStack.Text.SL5\bin\%BUILD%\*.* ..\..\ServiceStack\lib\sl5
+COPY ..\src\ServiceStack.Text.SL5\bin\%BUILD%\ServiceStack.Text.* ..\..\ServiceStack\lib\sl5
 
-MD ..\NuGet\lib\net35
-COPY ..\src\ServiceStack.Text\bin\%BUILD%\*.* ..\NuGet\lib\net35
-
-COPY ..\src\ServiceStack.Text\bin\Pcl\*.* ..\..\ServiceStack\lib\pcl
+COPY ..\src\ServiceStack.Text\bin\Pcl\ServiceStack.Text.* ..\..\ServiceStack\lib\pcl
