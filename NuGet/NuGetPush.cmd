3292d92e8f748c25ea77c464d0e82824fd5d50e5
Demis Bellot <demis.bellot@gmail.com>
2014-03-18 14:32:25 -0400

    Update NuGetPush to push all SS.Text pkgs

--- NuGet/NuGetPush.cmd
+++ NuGet/NuGetPush.cmd
@@ -1,3 +1,9 @@
 SET NUGET=..\src\.nuget\nuget
-%NUGET% push ServiceStack.Text.3.9.60.nupkg
-%NUGET% push ServiceStack.Text.3.9.60.symbols.nupkg
+%NUGET% push ServiceStack.Text.4.0.15.nupkg
+%NUGET% push ServiceStack.Text.4.0.15.symbols.nupkg
+
+%NUGET% push ServiceStack.Text.Pcl.4.0.15.nupkg
+%NUGET% push ServiceStack.Text.Pcl.4.0.15.symbols.nupkg
+
+%NUGET% push ServiceStack.Text.Signed.4.0.15.nupkg
+%NUGET% push ServiceStack.Text.Signed.4.0.15.symbols.nupkg
