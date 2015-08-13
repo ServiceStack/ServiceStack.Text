ae99783e172cea922b79e582f172937b56e8fe61
Demis Bellot <demis.bellot@gmail.com>
2015-08-14 01:44:27 +0800

    Add new TestConsole project

---
+++ tests/ServiceStack.Text.TestsConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceStack.Text.TestsConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var licenseKey = Environment.GetEnvironmentVariable("SERVICESTACK_LICENSE");
+            if (licenseKey.IsNullOrEmpty())
+                throw new ArgumentNullException("SERVICESTACK_LICENSE", "Add Environment variable for SERVICESTACK_LICENSE");
+
+            Licensing.RegisterLicense(licenseKey);
+            "ActivatedLicenseFeatures: ".Print(LicenseUtils.ActivatedLicenseFeatures());
+
+            Console.ReadLine();
+        }
+    }
+}
