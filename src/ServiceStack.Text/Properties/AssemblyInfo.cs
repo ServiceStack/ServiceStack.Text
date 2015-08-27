81dc4a35bf6d96e9e3f476e5e56889d36af396f0
Demis Bellot <demis.bellot@gmail.com>
2015-08-27 15:22:38 -0700

    check in updated renamed project.json

--- src/ServiceStack.Text/Properties/AssemblyInfo.cs
+++ src/ServiceStack.Text/Properties/AssemblyInfo.cs
@@ -19,7 +19,9 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+#if !PCL
 [assembly: Guid("a352d4d3-df2a-4c78-b646-67181a6333a6")]
+#endif
 
 // Version information for an assembly consists of the following four values:
 //
