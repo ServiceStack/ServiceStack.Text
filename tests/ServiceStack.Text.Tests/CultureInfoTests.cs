a030887fded8a7decdb8b2ce0cd629ef44cc6ad6
Demis Bellot <demis.bellot@gmail.com>
2015-09-25 14:53:06 -0700

    Move Point, NumberTypes to support

--- tests/ServiceStack.Text.Tests/CultureInfoTests.cs
+++ tests/ServiceStack.Text.Tests/CultureInfoTests.cs
@@ -2,41 +2,14 @@ using System;
 using System.Globalization;
 using System.Threading;
 using NUnit.Framework;
+using ServiceStack.Text.Tests.Support;
 
 namespace ServiceStack.Text.Tests
 {
-	[TestFixture]
+    [TestFixture]
 	public class CultureInfoTests
 		: TestBase
 	{
-		public class Point
-		{
-			public double Latitude { get; set; }
-			public double Longitude { get; set; }
-
-			public bool Equals(Point other)
-			{
-				if (ReferenceEquals(null, other)) return false;
-				if (ReferenceEquals(this, other)) return true;
-				return other.Latitude == Latitude && other.Longitude == Longitude;
-			}
-
-			public override bool Equals(object obj)
-			{
-				if (ReferenceEquals(null, obj)) return false;
-				if (ReferenceEquals(this, obj)) return true;
-				if (obj.GetType() != typeof(Point)) return false;
-				return Equals((Point)obj);
-			}
-
-			public override int GetHashCode()
-			{
-				unchecked
-				{
-					return (Latitude.GetHashCode() * 397) ^ Longitude.GetHashCode();
-				}
-			}
-		}
 
 		private CultureInfo previousCulture = CultureInfo.InvariantCulture;
 
