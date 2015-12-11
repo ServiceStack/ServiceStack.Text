57a26a9b0749baf7e805b165482f6af2af05b774
Demis Bellot <demis.bellot@gmail.com>
2015-12-11 04:51:59 -0800

    fix null property of generic collection

--- tests/ServiceStack.Text.Tests/JsonTests/BasicPropertiesTests.cs
+++ tests/ServiceStack.Text.Tests/JsonTests/BasicPropertiesTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 using NUnit.Framework;
 
@@ -171,6 +172,22 @@ namespace ServiceStack.Text.Tests.JsonTests
             var sb = new StringBuilder();
             foreach (var key in d.Keys) { sb.AppendLine(key + " = " + d[key]); }
             return sb.ToString();
+        }
+
+        public class ModelWithHashSet
+        {
+            public HashSet<string> Set { get; set; }
+        }
+
+        [Test]
+        public void Can_deserialize_null_Nested_HashSet()
+        {
+            JsConfig.ThrowOnDeserializationError = true;
+            string json = @"{""set"":null}";
+            var o = json.FromJson<ModelWithHashSet>();
+            Assert.That(o.Set, Is.Null);
+
+            JsConfig.Reset();
         }
     }
 }
