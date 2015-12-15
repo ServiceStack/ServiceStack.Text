895a60164cb39f9e2235a6c7995618c64fa212ef
Demis Bellot <demis.bellot@gmail.com>
2015-12-15 08:00:46 -0800

    Update replace code with the new DateTime initialization syntax

--- tests/ServiceStack.Text.TestsConsole/Program.cs
+++ tests/ServiceStack.Text.TestsConsole/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ServiceStack.Text.TestsConsole
@@ -10,12 +12,16 @@ namespace ServiceStack.Text.TestsConsole
     {
         static void Main(string[] args)
         {
-            var licenseKey = Environment.GetEnvironmentVariable("SERVICESTACK_LICENSE");
-            if (licenseKey.IsNullOrEmpty())
-                throw new ArgumentNullException("SERVICESTACK_LICENSE", "Add Environment variable for SERVICESTACK_LICENSE");
+            //var licenseKey = Environment.GetEnvironmentVariable("SERVICESTACK_LICENSE");
+            //if (licenseKey.IsNullOrEmpty())
+            //    throw new ArgumentNullException("SERVICESTACK_LICENSE", "Add Environment variable for SERVICESTACK_LICENSE");
 
-            Licensing.RegisterLicense(licenseKey);
-            "ActivatedLicenseFeatures: ".Print(LicenseUtils.ActivatedLicenseFeatures());
+            //Licensing.RegisterLicense(licenseKey);
+            //"ActivatedLicenseFeatures: ".Print(LicenseUtils.ActivatedLicenseFeatures());
+
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("ar");
+            JsConfig.InitStatics();
+            //JsonServiceClient client = new JsonServiceClient();
 
             Console.ReadLine();
         }
