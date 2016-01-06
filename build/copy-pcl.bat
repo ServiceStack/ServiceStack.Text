4507e0b2f58942b462ef8250275cfc00cec0bc88
Demis Bellot <demis.bellot@gmail.com>
2016-01-05 16:44:54 -0800

    Add AOT Helper code which also seems to help UWP .NET Native where the official rd.xml has no effect

--- build/copy-pcl.bat
+++ build/copy-pcl.bat
@@ -9,9 +9,11 @@ COPY ..\src\ServiceStack.Text\PclExport.Net40.cs		..\..\ServiceStack\src\Service
 COPY ..\src\ServiceStack.Text\PclExport.Net40.cs		..\..\ServiceStack\src\ServiceStack.Pcl.Ios\
 COPY ..\src\ServiceStack.Text\PclExport.Net40.cs		..\..\ServiceStack\src\ServiceStack.Pcl.Net45\
 COPY ..\src\ServiceStack.Text\PclExport.WinStore.cs		..\..\ServiceStack\src\ServiceStack.Pcl.WinStore\
+COPY ..\src\ServiceStack.Text\PclExport.WinStore.cs		..\..\ServiceStack\src\ServiceStack.Pcl.WinStore81\
 
 COPY ..\src\ServiceStack.Text\Pcl.*				..\..\ServiceStack\src\ServiceStack.Pcl.Android\
 COPY ..\src\ServiceStack.Text\Pcl.*				..\..\ServiceStack\src\ServiceStack.Pcl.Ios\
 COPY ..\src\ServiceStack.Text\Pcl.*				..\..\ServiceStack\src\ServiceStack.Pcl.Net45\
 COPY ..\src\ServiceStack.Text\Pcl.*				..\..\ServiceStack\src\ServiceStack.Pcl.WinStore\
+COPY ..\src\ServiceStack.Text\Pcl.*				..\..\ServiceStack\src\ServiceStack.Pcl.WinStore81\
 
