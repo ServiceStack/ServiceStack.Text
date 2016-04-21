56a50672c8f42761a9dddb11e6e335d8d256027c
Demis Bellot <demis.bellot@gmail.com>
2016-04-21 02:22:31 -0400

    Add StringBuilderCacheAlt when 2x StringBuilders are needed

--- src/ServiceStack.Text/StringBuilderCache.cs
+++ src/ServiceStack.Text/StringBuilderCache.cs
@@ -35,6 +35,35 @@ namespace ServiceStack.Text
         }
     }
 
+    public static class StringBuilderCacheAlt
+    {
+        [ThreadStatic]
+        static StringBuilder cache;
+
+        public static StringBuilder Allocate()
+        {
+            var ret = cache;
+            if (ret == null)
+                return new StringBuilder();
+
+            ret.Length = 0;
+            cache = null;  //don't re-issue cached instance until it's freed
+            return ret;
+        }
+
+        public static void Free(StringBuilder sb)
+        {
+            cache = sb;
+        }
+
+        public static string ReturnAndFree(StringBuilder sb)
+        {
+            var ret = sb.ToString();
+            cache = sb;
+            return ret;
+        }
+    }
+
     //Use separate cache internally to avoid reallocations and cache misses
     internal static class StringBuilderThreadStatic
     {
