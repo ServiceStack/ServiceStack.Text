c2f3313801680b54d9dc739b1250aba8259c18a8
Demis Bellot <demis.bellot@gmail.com>
2016-06-02 11:04:05 -0400

    Add useful ToBase64UrlSafe/FromBase64UrlSafe from JWT spec

--- src/ServiceStack.Text/StringExtensions.cs
+++ src/ServiceStack.Text/StringExtensions.cs
@@ -321,6 +321,33 @@ namespace ServiceStack
             return FastToUtf8Bytes(doubleStr);
         }
 
+        // from JWT spec
+        public static string ToBase64UrlSafe(this byte[] input)
+        {
+            var output = Convert.ToBase64String(input);
+            output = output.Split('=')[0]; // Remove any trailing '='s
+            output = output.Replace('+', '-'); // 62nd char of encoding
+            output = output.Replace('/', '_'); // 63rd char of encoding
+            return output;
+        }
+
+        // from JWT spec
+        public static byte[] FromBase64UrlSafe(this string input)
+        {
+            var output = input;
+            output = output.Replace('-', '+'); // 62nd char of encoding
+            output = output.Replace('_', '/'); // 63rd char of encoding
+            switch (output.Length % 4) // Pad with trailing '='s
+            {
+                case 0: break; // No pad chars in this case
+                case 2: output += "=="; break; // Two pad chars
+                case 3: output += "="; break;  // One pad char
+                default: throw new Exception("Illegal base64url string!");
+            }
+            var converted = Convert.FromBase64String(output); // Standard base64 decoder
+            return converted;
+        }
+
         /// <summary>
         /// Skip the encoding process for 'safe strings' 
         /// </summary>
