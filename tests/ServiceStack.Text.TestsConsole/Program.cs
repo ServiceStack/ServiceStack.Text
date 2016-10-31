6d718b10421a288beeee2eff2736d3f131784fe2
Demis Bellot <demis.bellot@gmail.com>
2016-10-31 13:12:59 -0400

    clear old testconsole

--- tests/ServiceStack.Text.TestsConsole/Program.cs
+++ tests/ServiceStack.Text.TestsConsole/Program.cs
@@ -1,29 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace ServiceStack.Text.TestsConsole
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //var licenseKey = Environment.GetEnvironmentVariable("SERVICESTACK_LICENSE");
-            //if (licenseKey.IsNullOrEmpty())
-            //    throw new ArgumentNullException("SERVICESTACK_LICENSE", "Add Environment variable for SERVICESTACK_LICENSE");
 
-            //Licensing.RegisterLicense(licenseKey);
-            //"ActivatedLicenseFeatures: ".Print(LicenseUtils.ActivatedLicenseFeatures());
-
-            Thread.CurrentThread.CurrentCulture = new CultureInfo("ar");
-            JsConfig.InitStatics();
-            //JsonServiceClient client = new JsonServiceClient();
-
-            Console.ReadLine();
-        }
-    }
 }
