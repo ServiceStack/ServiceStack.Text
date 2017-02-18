d96205605920ba86352a8903add60cac658a08a8
Demis Bellot <demis.bellot@gmail.com>
2017-02-18 01:42:26 -0500

    Add JSV Float tests

---
+++ tests/ServiceStack.Text.Tests/JsvTests/JsvBasicDataTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using ServiceStack.Text.Tests.Shared;
+
+namespace ServiceStack.Text.Tests.JsvTests
+{
+    public class JsvBasicDataTests
+    {
+        [Test]
+        public void Can_serialize_ModelWithFloatTypes()
+        {
+            var dto = new ModelWithFloatTypes
+            {
+                Float = 1.1f,
+                Double = 2.2d,
+                Decimal = 3.3m
+            };
+
+            var jsv = dto.ToJsv();
+            Assert.That(jsv,Is.EqualTo("{Float:1.1,Double:2.2,Decimal:3.3}"));
+
+            var fromJsv = jsv.FromJsv<ModelWithFloatTypes>();
+            Assert.That(fromJsv, Is.EqualTo(dto));
+
+            dto = new ModelWithFloatTypes
+            {
+                Float   = 1111111.11f,
+                Double  = 2222222.22d,
+                Decimal = 33333333.33m
+            };
+
+            jsv = dto.ToJsv();
+            Assert.That(jsv, Is.EqualTo("{Float:1111111,Double:2222222.22,Decimal:33333333.33}"));
+
+            fromJsv = jsv.FromJsv<ModelWithFloatTypes>();
+            Assert.That(fromJsv, Is.EqualTo(dto));
+        }
+    }
+}
