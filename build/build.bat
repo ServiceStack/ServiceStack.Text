5c4cabd6a2f908f9baba1a5fa88c9c5e750e1b89
Demis Bellot <demis.bellot@gmail.com>
2017-04-05 12:43:17 -0400

    Change to point to new VS2017 msbuild

--- build/build.bat
+++ build/build.bat
@@ -1,4 +1,4 @@
-SET MSBUILD=C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
+SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
 
 %MSBUILD% build.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;RELEASE=true;PatchVersion=0
 REM %MSBUILD% build-sn.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Signed;RELEASE=true;PatchVersion=1
