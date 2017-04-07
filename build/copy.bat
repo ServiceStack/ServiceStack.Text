cf83c0f3a9a3a1519286f674c13f0e0215e04296
Demis Bellot <demis.bellot@gmail.com>
2017-04-07 05:04:54 -0400

    Update copy.bat to copy to all projs that need them

--- build/copy.bat
+++ build/copy.bat
@@ -6,16 +6,30 @@ COPY ..\src\ServiceStack.Text\bin\%BUILD%\net45\ServiceStack.Text.* ..\..\Servic
 COPY ..\src\ServiceStack.Text\bin\%BUILD%\net45\ServiceStack.Text.* ..\..\ServiceStack.Contrib\lib
 COPY ..\src\ServiceStack.Text\bin\%BUILD%\net45\ServiceStack.Text.* ..\..\ServiceStack.Redis\lib
 COPY ..\src\ServiceStack.Text\bin\%BUILD%\net45\ServiceStack.Text.* ..\..\ServiceStack.OrmLite\lib
+COPY ..\src\ServiceStack.Text\bin\%BUILD%\net45\ServiceStack.Text.* ..\..\ServiceStack.Aws\lib
+COPY ..\src\ServiceStack.Text\bin\%BUILD%\net45\ServiceStack.Text.* ..\..\ServiceStack.Admin\lib
 COPY ..\src\ServiceStack.Text\bin\%BUILD%\net45\ServiceStack.Text.* ..\..\Stripe\lib
-COPY "..\src\ServiceStack.Text\bin\%BUILD%\portable45-net45+win8\ServiceStack.Text.*" ..\..\Stripe\lib\pcl
 
 COPY ..\src\ServiceStack.Text\bin\Signed\net45\ServiceStack.Text.* ..\..\ServiceStack\lib\signed
 COPY ..\src\ServiceStack.Text\bin\Signed\net45\ServiceStack.Text.* ..\..\ServiceStack.Redis\lib\signed
 COPY ..\src\ServiceStack.Text\bin\Signed\net45\ServiceStack.Text.* ..\..\ServiceStack.OrmLite\lib\signed
-
-COPY ..\src\ServiceStack.Text.SL5\bin\SL5\ServiceStack.Text.* ..\..\ServiceStack\lib\sl5
-
-COPY "..\src\ServiceStack.Text\bin\%BUILD%\portable45-net45+win8\ServiceStack.Text.dll" ..\..\ServiceStack\lib\pcl
+COPY ..\src\ServiceStack.Text\bin\Signed\net45\ServiceStack.Text.* ..\..\ServiceStack.Aws\lib\signed
+COPY ..\src\ServiceStack.Text\bin\Signed\net45\ServiceStack.Text.* ..\..\ServiceStack.Admin\lib\signed
 
 COPY ..\src\ServiceStack.Text\bin\%BUILD%\netstandard1.1\*.* ..\..\ServiceStack\lib\netstandard1.1
 COPY ..\src\ServiceStack.Text\bin\%BUILD%\netstandard1.3\*.* ..\..\ServiceStack\lib\netstandard1.3
+COPY ..\src\ServiceStack.Text\bin\%BUILD%\netstandard1.1\*.* ..\..\ServiceStack.Redis\lib\netstandard1.1
+COPY ..\src\ServiceStack.Text\bin\%BUILD%\netstandard1.3\*.* ..\..\ServiceStack.Redis\lib\netstandard1.3
+COPY ..\src\ServiceStack.Text\bin\%BUILD%\netstandard1.1\*.* ..\..\ServiceStack.OrmLite\lib\netstandard1.1
+COPY ..\src\ServiceStack.Text\bin\%BUILD%\netstandard1.3\*.* ..\..\ServiceStack.OrmLite\lib\netstandard1.3
+COPY ..\src\ServiceStack.Text\bin\%BUILD%\netstandard1.1\*.* ..\..\ServiceStack.Aws\lib\netstandard1.1
+COPY ..\src\ServiceStack.Text\bin\%BUILD%\netstandard1.3\*.* ..\..\ServiceStack.Aws\lib\netstandard1.3
+COPY ..\src\ServiceStack.Text\bin\%BUILD%\netstandard1.1\*.* ..\..\Admin\lib\netstandard1.1
+COPY ..\src\ServiceStack.Text\bin\%BUILD%\netstandard1.3\*.* ..\..\Admin\lib\netstandard1.3
+COPY ..\src\ServiceStack.Text\bin\%BUILD%\netstandard1.1\*.* ..\..\Stripe\lib\netstandard1.1
+COPY ..\src\ServiceStack.Text\bin\%BUILD%\netstandard1.3\*.* ..\..\Stripe\lib\netstandard1.3
+
+COPY "..\src\ServiceStack.Text\bin\%BUILD%\portable45-net45+win8\ServiceStack.Text.dll" ..\..\ServiceStack\lib\pcl
+COPY "..\src\ServiceStack.Text\bin\%BUILD%\portable45-net45+win8\ServiceStack.Text.*" ..\..\Stripe\lib\pcl
+
+COPY ..\src\ServiceStack.Text.SL5\bin\SL5\ServiceStack.Text.* ..\..\ServiceStack\lib\sl5
