136bf63a99b69b0013a4957f5174f531a4b34022
Demis Bellot <demis.bellot@gmail.com>
2017-04-07 04:38:49 -0400

    update signed settings

--- build/copy.bat
+++ build/copy.bat
@@ -7,14 +7,15 @@ COPY ..\src\ServiceStack.Text\bin\%BUILD%\net45\ServiceStack.Text.* ..\..\Servic
 COPY ..\src\ServiceStack.Text\bin\%BUILD%\net45\ServiceStack.Text.* ..\..\ServiceStack.Redis\lib
 COPY ..\src\ServiceStack.Text\bin\%BUILD%\net45\ServiceStack.Text.* ..\..\ServiceStack.OrmLite\lib
 COPY ..\src\ServiceStack.Text\bin\%BUILD%\net45\ServiceStack.Text.* ..\..\Stripe\lib
+COPY "..\src\ServiceStack.Text\bin\%BUILD%\portable45-net45+win8\ServiceStack.Text.*" ..\..\Stripe\lib\pcl
 
-COPY ..\src\ServiceStack.Text\bin\Release\Signed\ServiceStack.Text.* ..\..\ServiceStack\lib\signed
-COPY ..\src\ServiceStack.Text\bin\Release\Signed\ServiceStack.Text.* ..\..\ServiceStack.Redis\lib\signed
-COPY ..\src\ServiceStack.Text\bin\Release\Signed\ServiceStack.Text.* ..\..\ServiceStack.OrmLite\lib\signed
+COPY ..\src\ServiceStack.Text\bin\Signed\net45\ServiceStack.Text.* ..\..\ServiceStack\lib\signed
+COPY ..\src\ServiceStack.Text\bin\Signed\net45\ServiceStack.Text.* ..\..\ServiceStack.Redis\lib\signed
+COPY ..\src\ServiceStack.Text\bin\Signed\net45\ServiceStack.Text.* ..\..\ServiceStack.OrmLite\lib\signed
 
 COPY ..\src\ServiceStack.Text.SL5\bin\SL5\ServiceStack.Text.* ..\..\ServiceStack\lib\sl5
 
-COPY ..\src\ServiceStack.Text\bin\%BUILD%\portable45-net45+win8\ServiceStack.Text.dll ..\..\ServiceStack\lib\pcl
+COPY "..\src\ServiceStack.Text\bin\%BUILD%\portable45-net45+win8\ServiceStack.Text.dll" ..\..\ServiceStack\lib\pcl
 
 COPY ..\src\ServiceStack.Text\bin\%BUILD%\netstandard1.1\*.* ..\..\ServiceStack\lib\netstandard1.1
 COPY ..\src\ServiceStack.Text\bin\%BUILD%\netstandard1.3\*.* ..\..\ServiceStack\lib\netstandard1.3
