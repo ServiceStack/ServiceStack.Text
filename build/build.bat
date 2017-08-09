5c50b6d07c6e383b40b35a08f143d292372aca1d
Sergey Zhukov <svg@ngs.ru>
2017-08-09 12:47:02 +0700

    Fix pcl/sl5 on VS 2017 15.3 msbuild toolset

--- build/build.bat
+++ build/build.bat
@@ -1,6 +1,8 @@
-SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
+SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\Preview\Community\MSBuild\15.0\Bin\MSBuild.exe"
 
 REM %MSBUILD% build-sn.proj /target:NuGetPack /property:Configuration=Signed;RELEASE=true;PatchVersion=9
 %MSBUILD% build-core.proj /target:NuGetPack /property:Configuration=Release;PatchVersion=41
 %MSBUILD% build.proj /target:NuGetPack /property:Configuration=Release;PatchVersion=9
+%MSBUILD% build-pcl.proj /target:NuGetPack /property:Configuration=Release;PatchVersion=9
+%MSBUILD% build-sl5.proj /target:NuGetPack /property:Configuration=Release;PatchVersion=9
 
