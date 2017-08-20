ef0d0dcbb10471a80d977b0b8dc28263883c04e6
Demis Bellot <demis.bellot@gmail.com>
2017-08-20 02:58:58 -0400

    modify build.bat

--- build/build.bat
+++ build/build.bat
@@ -1,8 +1,10 @@
 SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\Preview\Community\MSBuild\15.0\Bin\MSBuild.exe"
 
 REM %MSBUILD% build-sn.proj /target:NuGetPack /property:Configuration=Signed;RELEASE=true;PatchVersion=9
-%MSBUILD% build-core.proj /target:NuGetPack /property:Configuration=Release;PatchVersion=41
-%MSBUILD% build.proj /target:NuGetPack /property:Configuration=Release;PatchVersion=9
-%MSBUILD% build-pcl.proj /target:NuGetPack /property:Configuration=Release;PatchVersion=9
-%MSBUILD% build-sl5.proj /target:NuGetPack /property:Configuration=Release;PatchVersion=9
+REM %MSBUILD% build-core.proj /target:NuGetPack /property:Configuration=Release;PatchVersion=41
+REM %MSBUILD% build.proj /target:NuGetPack /property:Configuration=Release;PatchVersion=9
+REM %MSBUILD% build-pcl.proj /target:NuGetPack /property:Configuration=Release;PatchVersion=9
+REM %MSBUILD% build-sl5.proj /target:NuGetPack /property:Configuration=Release;PatchVersion=9
+
+msbuild /p:Configuration=Release ..\src\ServiceStack.Text.sln
 
