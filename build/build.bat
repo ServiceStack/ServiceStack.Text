b2c66e115760c6e1f2e56b942e0d847ab22c6816
Demis Bellot <demis.bellot@gmail.com>
2017-12-14 21:12:21 -0500

    fix refs

--- build/build.bat
+++ build/build.bat
@@ -1,10 +1,6 @@
-SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\Preview\Community\MSBuild\15.0\Bin\MSBuild.exe"
+SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
 
-REM %MSBUILD% build-sn.proj /target:NuGetPack /property:Configuration=Signed;RELEASE=true;PatchVersion=9
-REM %MSBUILD% build-core.proj /target:NuGetPack /property:Configuration=Release;PatchVersion=41
-REM %MSBUILD% build.proj /target:NuGetPack /property:Configuration=Release;PatchVersion=9
-REM %MSBUILD% build-pcl.proj /target:NuGetPack /property:Configuration=Release;PatchVersion=9
-REM %MSBUILD% build-sl5.proj /target:NuGetPack /property:Configuration=Release;PatchVersion=9
+%MSBUILD% build.proj /target:UpdateVersion /property:Configuration=Release;MinorVersion=1;PatchVersion=0
 
-msbuild /p:Configuration=Release ..\src\ServiceStack.Text.sln
+REM msbuild /p:Configuration=Release ..\src\ServiceStack.Text.sln
 
