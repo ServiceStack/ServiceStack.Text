f7fd6ec2b8b8ecb41a895b933b3e421459e47d35
Demis Bellot <demis.bellot@gmail.com>
2018-01-01 23:26:20 -0500

    Add Span Example in test proj

---
+++ tests/ServiceStack.Text.Tests/SpanTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ServiceStack.Text.Tests
+{
+    public class SpanTests
+    {
+        [Test]
+        public void Can_use_Span()
+        {
+            //.AsReadOnlySpan() not available in System.Memory v4.4.0-preview1-25305-02 on NuGet yet
+            ReadOnlySpan<char> a = "foo bar".AsSpan();
+
+            var foo = a.Slice(0,3).ToArray();
+
+            Assert.That(foo, Is.EqualTo("foo".ToCharArray()));
+        }
+    }
+}
