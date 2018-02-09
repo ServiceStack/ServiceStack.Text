a6d4d15fcc6082ce40a0d467bdb701ad516e23c8
¯\_(ツ)_/¯ <Siliconrob@siliconheaven.net>
2018-02-09 13:54:20 -0600

    CSV DateTime formatting for midnight fix
    DateTimes that are exported in ISO8601 are not consistent across serialization for midnight on days.
    
    JSON and XML both show a datetime with the fractional seconds like so
    
    `2018-02-10T00:00:00.0000000Z`
    
    but CSV will produce a DateTime with out the any of the timespan portion of timezone indicator i.e.
    
    `2018-01-01`
    
    this is also not consistent with the default ISO8601 parser in .NET for DateTime called as so `.ToString("o")` will produce the ISO8601 serialized times as for JSON and XML, but not CSV.
    
    Adding the ability to specify a DateTimeFormat that will allow a user to override the DateTimeFormat defaults so serialized value is consistent

--- src/ServiceStack.Text/JsConfig.cs
+++ src/ServiceStack.Text/JsConfig.cs
@@ -222,6 +222,7 @@ namespace ServiceStack.Text
             bool? preferInterfaces = null,
             bool? throwOnDeserializationError = null,
             string typeAttr = null,
+            string dateTimeFormat = null,
             Func<Type, string> typeWriter = null,
             Func<string, Type> typeFinder = null,
             bool? treatEnumAsInteger = null,
@@ -258,6 +259,7 @@ namespace ServiceStack.Text
                 PropertyConvention = propertyConvention ?? sPropertyConvention,
                 PreferInterfaces = preferInterfaces ?? sPreferInterfaces,
                 ThrowOnDeserializationError = throwOnDeserializationError ?? sThrowOnDeserializationError,
+                DateTimeFormat = dateTimeFormat ?? sDateTimeFormat,
                 TypeAttr = typeAttr ?? sTypeAttr,
                 TypeWriter = typeWriter ?? sTypeWriter,
                 TypeFinder = typeFinder ?? sTypeFinder,
@@ -272,6 +274,21 @@ namespace ServiceStack.Text
                 ModelFactory = modelFactory ?? ModelFactory,
                 ExcludePropertyReferences = excludePropertyReferences ?? sExcludePropertyReferences,
             };
+        }
+
+        private static string sDateTimeFormat;
+        public static string DateTimeFormat
+        {
+            get
+            {
+                return (JsConfigScope.Current != null ? JsConfigScope.Current.DateTimeFormat : null)
+                       ?? sDateTimeFormat;
+                       //?? DateTimeSerializer.XsdDateTimeFormatSeconds;
+            }
+            set
+            {
+                if (sDateTimeFormat == null) sDateTimeFormat = value;
+            }
         }
 
         private static bool? sConvertObjectTypesIntoStringDictionary;
@@ -974,6 +991,7 @@ namespace ServiceStack.Text
             sPreferInterfaces = null;
             sThrowOnDeserializationError = null;
             sTypeAttr = null;
+            sDateTimeFormat = null;
             sJsonTypeAttrInObject = null;
             sJsvTypeAttrInObject = null;
             sTypeWriter = null;
