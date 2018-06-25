458f413fd38cbb0afac720b65c0e5d304f9537a1
Demis Bellot <demis.bellot@gmail.com>
2018-06-24 23:55:17 -0400

    Add more UTF-8 test cases

--- tests/ServiceStack.Text.Tests/SpanTests.cs
+++ tests/ServiceStack.Text.Tests/SpanTests.cs
@@ -153,8 +153,52 @@ namespace ServiceStack.Text.Tests
 
     public class Utf8Case
     {
-        public static Utf8Case[] Source = new[] {
-            new Utf8Case(new byte[] {70, 111, 111, 66, 65, 208, 128, 82}, 0, 8, "FooBA\u0400R"),                
+        //https://github.com/dotnet/corefx/blob/master/src/System.Text.Encoding/tests/UTF8Encoding/UTF8EncodingDecode.cs
+        public static readonly Utf8Case[] Source = {
+            new Utf8Case(new byte[] {70, 111, 111, 66, 65, 208, 128, 82}, 0, 8, "FooBA\u0400R"),
+            new Utf8Case(new byte[] { 195, 128, 110, 105, 109, 97, 204, 128, 108 }, 0, 9, "\u00C0nima\u0300l"),
+            new Utf8Case(new byte[] { 84, 101, 115, 116, 240, 144, 181, 181, 84, 101, 115, 116 }, 0, 12, "Test\uD803\uDD75Test"),
+            new Utf8Case(new byte[] { 0, 84, 101, 10, 115, 116, 0, 9, 0, 84, 15, 101, 115, 116, 0 }, 0, 15, "\0Te\nst\0\t\0T\u000Fest\0"),
+            new Utf8Case(new byte[] { 240, 144, 181, 181, 240, 144, 181, 181, 240, 144, 181, 181 }, 0, 12, "\uD803\uDD75\uD803\uDD75\uD803\uDD75"),
+            new Utf8Case(new byte[] { 196, 176 }, 0, 2, "\u0130"),
+            new Utf8Case(new byte[] { 0x61, 0xCC, 0x8A }, 0, 3, "\u0061\u030A"),
+            new Utf8Case(new byte[] { 0xC2, 0xA4, 0xC3, 0x90, 0x61, 0x52, 0x7C, 0x7B, 0x41, 0x6E, 0x47, 0x65, 0xC2, 0xA3, 0xC2, 0xA4 }, 0, 16, "\u00A4\u00D0aR|{AnGe\u00A3\u00A4"),
+            new Utf8Case(new byte[] { 0x00, 0x7F }, 0, 2, "\u0000\u007F"),
+            new Utf8Case(new byte[] { 0x00, 0x7F, 0x00, 0x7F, 0x00, 0x7F, 0x00, 0x7F, 0x00, 0x7F, 0x00, 0x7F, 0x00, 0x7F }, 0, 14, "\u0000\u007F\u0000\u007F\u0000\u007F\u0000\u007F\u0000\u007F\u0000\u007F\u0000\u007F"),
+            new Utf8Case(new byte[] { 0xC2, 0x80, 0xDF, 0xBF }, 0, 4, "\u0080\u07FF"),
+            
+            // Long ASCII strings
+            new Utf8Case(new byte[] { 84, 101, 115, 116, 83, 116, 114, 105, 110, 103 }, 0, 10, "TestString"),
+            new Utf8Case(new byte[] { 84, 101, 115, 116, 84, 101, 115, 116 }, 0, 8, "TestTest"),
+            
+            // Control codes
+            new Utf8Case(new byte[] { 0x1F, 0x10, 0x00, 0x09 }, 0, 4, "\u001F\u0010\u0000\u0009"),
+            new Utf8Case(new byte[] { 0x1F, 0x00, 0x10, 0x09 }, 0, 4, "\u001F\u0000\u0010\u0009"),
+            new Utf8Case(new byte[] { 0x00, 0x1F, 0x10, 0x09 }, 0, 4, "\u0000\u001F\u0010\u0009"),
+            
+            // BOM
+            new Utf8Case(new byte[] { 0xEF, 0xBB, 0xBF, 0x41 }, 0, 4, "\uFEFF\u0041"),
+            
+            // U+FDD0 - U+FDEF
+            new Utf8Case(new byte[] { 0xEF, 0xB7, 0x90, 0xEF, 0xB7, 0xAF }, 0, 6, "\uFDD0\uFDEF"),
+            
+            // 2 byte encoding
+            new Utf8Case(new byte[] { 0xC3, 0xA1 }, 0, 2, "\u00E1"),
+            new Utf8Case(new byte[] { 0xC3, 0x85 }, 0, 2, "\u00C5"),
+            
+            // 3 byte encoding
+            new Utf8Case(new byte[] { 0xE8, 0x80, 0x80 }, 0, 3, "\u8000"),
+            new Utf8Case(new byte[] { 0xE2, 0x84, 0xAB }, 0, 3, "\u212B"),
+            
+            // Surrogate pairs
+            new Utf8Case(new byte[] { 240, 144, 128, 128 }, 0, 4, "\uD800\uDC00"),
+            new Utf8Case(new byte[] { 97, 240, 144, 128, 128, 98 }, 0, 6, "a\uD800\uDC00b"), 
+            
+            // High BMP non-chars
+            new Utf8Case(new byte[] { 239, 191, 189 }, 0, 3, "\uFFFD"),
+            
+            // Empty strings
+            new Utf8Case(new byte[0], 0, 0, string.Empty),
         };
 
         public byte[] expectedBytes;
