9abe46e462a751f93b505d5f527e04865ccf94d1
Demis Bellot <demis.bellot@gmail.com>
2018-12-15 02:53:20 +0800

    Update Version to v5.4.1 to use CI NuGet packages

--- build/build.bat
+++ build/build.bat
@@ -1,3 +1,3 @@
 SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
 
-%MSBUILD% build.proj /property:Configuration=Release;MinorVersion=1;PatchVersion=1
+%MSBUILD% build.proj /property:Configuration=Release;MinorVersion=4;PatchVersion=1
