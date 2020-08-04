f4f252f7469662d0795f1c8bf32e20ab645aea4d
Demis Bellot <demis.bellot@gmail.com>
2020-08-04 18:26:34 +0800

    fix typo

--- src/ServiceStack.Text/Env.cs
+++ src/ServiceStack.Text/Env.cs
@@ -202,7 +202,7 @@ namespace ServiceStack.Text
                         {
                             throw new FileNotFoundException(
                                 "Could not infer .NET Reference Assemblies path, e.g '{0}'.\n".Fmt(netFxReferenceBasePath + @"v4.0\") +
-                                "Provide path manually 'Env.ReferenceAssembyPath'.");
+                                "Provide path manually 'Env.ReferenceAssemblyPath'.");
                         }
                     }
                 }
