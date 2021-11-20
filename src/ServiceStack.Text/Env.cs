cb3bd5cab3fcf71e0c44a22628f92d020f904acc
Demis Bellot <demis.bellot@gmail.com>
2021-11-20 14:10:43 +0800

    bump to v5.13.2

--- src/ServiceStack.Text/Env.cs
+++ src/ServiceStack.Text/Env.cs
@@ -116,7 +116,7 @@ namespace ServiceStack.Text
 
         public static string VersionString { get; set; }
 
-        public static decimal ServiceStackVersion = 5.131m;
+        public static decimal ServiceStackVersion = 5.132m;
 
         public static bool IsLinux { get; set; }
 
