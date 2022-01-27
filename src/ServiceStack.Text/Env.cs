296c7a84bdc18f9671834c5806d610b67a9f2faa
Demis Bellot <demis.bellot@gmail.com>
2022-01-27 19:34:13 +0800

    Update Env.cs

--- src/ServiceStack.Text/Env.cs
+++ src/ServiceStack.Text/Env.cs
@@ -117,7 +117,7 @@ namespace ServiceStack.Text
 
         public static string VersionString { get; set; }
 
-        public static decimal ServiceStackVersion = 6.00m;
+        public static decimal ServiceStackVersion = 6.01m;
 
         public static bool IsLinux { get; set; }
 
